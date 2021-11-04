cc170a5610b0686dddf2fb0bb7984bc3bb6c0f46
Kimberly Abigail Herman <kimberlyabigailh26@gmail.com>
2021-11-04 22:25:12 +0700

    Laravel BWA Store
    admin area

---
+++ bwastore-laravel/resources/views/includes/navbar-auth.blade.php
@@ -0,0 +1,39 @@
+<!-- Navigation -->
+    <nav
+      class="
+        navbar navbar-expand-lg navbar-light navbar-store
+        fixed-top
+        navbar-fixed-top
+      "
+      data-aos="fade-down"
+    >
+      <div class="container">
+        <a href="{{  url('home') }}" class="navbar-brand">
+          <img src="/images/logo.svg" alt="" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a href="{{  url('home') }}" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{  url('categories') }}" class="nav-link">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link" >Rewards</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
